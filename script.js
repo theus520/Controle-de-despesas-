0a6c14696aa2e279e85631926c67bca1b37b3dae
Matheus rabelo <matheusrabelo520@gmail.com>
2024-01-17 22:41:47 -0300

    Implementando a funcionalidade de cadastro de transação com a validação do formulário

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const transactionsUl = document.querySelector('#transactions')
 const incomeDisplay = document.querySelector('#money-plus')
 const expenseDisplay = document.querySelector('#money-minus')
 const balanceDisplay = document.querySelector('#balance')
+const form = document.querySelector('#form')
+const inputTransactionName = document.querySelector('#text')
+const inputTransactionAmount = document.querySelector('#amount')
 
 const dummyTransactions = [
     { id: 1, name: 'bolo de brigadeiro', amount: -20 },
@@ -50,9 +53,36 @@ const updateBalanceValues = () => {
 }
 
 const init = () => {
+
+    transactionsUl.innerHTML = ''
     dummyTransactions.forEach(addTransactionIntoDOM)
     updateBalanceValues()
 
 }
 
 init();
+
+const generateID = () => Math.round(Math.random() * 1000)
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const transactionName = inputTransactionName.value.trim()
+    const transactionAmount = inputTransactionAmount.value.trim()
+    
+    if (transactionName === '' || transactionAmount === ''){
+        alert('Por favor preencha tanto o nome quanto o valor ad transação')
+        return 
+    }
+
+  const transaction = { id: generateID(), 
+    name: transactionName , 
+    amount: Number(transactionAmount) 
+}
+
+dummyTransactions.push(transaction)
+init()
+
+inputTransactionName.value = ''
+inputTransactionAmount.value = ''
+})
