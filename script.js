f750849d83e510962d4d3e9624408e0f4a4b2e78
Matheus rabelo <matheusrabelo520@gmail.com>
2024-01-17 21:57:56 -0300

    Funcionalidade que insere o saldo total, receitas e despesas

---
+++ script.js
@@ -0,0 +1,48 @@
+const transactionsUl = document.querySelector('#transactions')
+
+const dummyTransactions = [
+    { id: 1, name: 'bolo de brigadeiro', amount: -20 },
+    { id: 2, name: 'Salário', amount: 300 },
+    { id: 3, name: 'Torta de frango', amount: -10 },
+    { id: 4, name: 'Violão', amount: 150 }
+]
+
+const addTransactionIntoDOM = transaction => {
+
+    const operator = transaction.amount < 0 ? '-' : '+'
+    const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
+    const amountWithoutOperator = Math.abs(transaction.amount)
+    const li = document.createElement('li')
+
+    li.classList.add(CSSClass)
+
+    li.innerHTML = `
+  ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}
+   </span><button class="delete-btn">x</button>
+
+   `
+    transactionsUl.append(li)
+}
+
+const updateBalanceValues = () => {
+    const transactionAmounts = dummyTransactions
+    .map(transaction => transaction.amount)
+    const total = transactionAmounts
+    .reduce((accumalator,transaction) => accumalator + transaction,0)
+    .toFixed(2)
+    const income = transactionAmounts
+    .filter(value => value > 0 )
+    .reduce((accumalator, value) => accumalator + value, 0)
+    .toFixed(2)
+
+    console.log(income);
+
+}
+
+const init = () => {
+    dummyTransactions.forEach(addTransactionIntoDOM)
+    updateBalanceValues()
+
+}
+
+init();
